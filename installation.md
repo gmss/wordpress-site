df621038a78e7701fafd6fd91cae6821606fa3b6
Andrew Taylor <mac@andrewt.net>
2014-11-26 00:44:36 +0000

    getting pretty swish now

--- installation.md
+++ installation.md
@@ -1,4 +1,5 @@
 1. Clone or copy gmss.uk repo into folder
 2. Restore database from backup
 3. Copy wp-config.php from WordPress 4.0.1 (or whatever we're using) and update to match your server. If for public consumption, use existing wp-config.php or, if none is available, update the salt keys for added security.
-4. Activate plugins and set theme, if needed.
+4. Correct the URL setting in the DB.
+5. Activate plugins and set theme, if needed.
--- wp-content/themes/gmss/header.php
+++ wp-content/themes/gmss/header.php
@@ -22,15 +22,18 @@
 				<li><a href="http://www.twitter.com/GMSkeptics">Twitter</a></li>
 			</ul>
 		</nav></header>
-		<nav class="tabs"><ul>
-			<li>
-				<a href="/">About</a>
-			</li><li>
-				<a href="/" class="active">Blog</a>
-			</li><li>
-				<a href="/events">Events</a>
-			</li><li>
-				<a href="#">Podcast</a>
-			</li>
-		</ul></nav>
+		<nav class="tabs"><ul><?php
+			$found = false;
+			foreach (array(
+							(object) array('url' => '/about', 'name' => 'About', 'active' => is_page(16)),
+							(object) array('url' => '/events', 'name' => 'Events',
+								'active' => preg_match('/^\/events($|\/)/i', $_SERVER['REQUEST_URI'])),
+							(object) array('url' => '/podcast', 'name' => 'Podcast', 'active' => false),
+							(object) array('url' => '/', 'name' => 'Blog', 'active' => true)
+						) as $tab) {
+				$active = $tab->active && !$found;
+				$found = $found || $active;
+				echo '<li><a href="' . $tab->url . '"' . ($active ? ' class="active"' : '') . '>' . $tab->name . '</a></li>';
+			}
+		?></ul></nav>
 		<div class="body">
