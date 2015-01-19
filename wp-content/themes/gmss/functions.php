d11809968ae408c96a7ab129f98c64876a62f54e
Andrew Taylor <mac@andrewt.net>
2015-01-19 23:05:44 +0000

    reverse order of evens listing

---
+++ wp-content/themes/gmss/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+// From https://wordpress.org/support/topic/plugin-event-organiser-archive-page-change-order-to-descending#post-3091586
+// Fixes order of events on all events page to show recent first.
+add_action('pre_get_posts','wpse50761_alter_query',5);
+function wpse50761_alter_query($query) {
+	if ($query->is_main_query() && is_post_type_archive('event')) {
+		$query->set('orderby', 'eventstart');
+		$query->set('order', 'desc');
+	}
+}
+
+?>
