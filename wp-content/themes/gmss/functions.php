535290e79e8ab36612f1ff7911b6e4657dfcc611
Andrew Taylor <mac@andrewt.net>
2015-01-30 21:56:51 +0000

    Event categories now sorted sensibly

--- wp-content/themes/gmss/functions.php
+++ wp-content/themes/gmss/functions.php
@@ -4,7 +4,9 @@
 // Fixes order of events on all events page to show recent first.
 add_action('pre_get_posts','wpse50761_alter_query',5);
 function wpse50761_alter_query($query) {
-	if ($query->is_main_query() && is_post_type_archive('event')) {
+	if ($query->is_main_query() &&
+			(is_post_type_archive('event') ||
+			preg_match('/^\/events($|\/)/i', $_SERVER['REQUEST_URI']))) {
 		$query->set('orderby', 'eventstart');
 		$query->set('order', 'desc');
 	}
