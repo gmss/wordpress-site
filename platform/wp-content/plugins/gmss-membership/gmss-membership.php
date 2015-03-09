42c97a2ea1d4cbf50dc59c158e6a96486fbb41aa
Andrew Taylor <mac@andrewt.net>
2015-03-09 22:48:22 +0000

    Revert "Split membership from newsletter? Not sure about this."
    This reverts commit 6aeb7e5781e262fe4cd2d1bc15bbf192f64d8477.

--- platform/wp-content/plugins/gmss-membership/gmss-membership.php
+++ platform/wp-content/plugins/gmss-membership/gmss-membership.php
@@ -17,22 +17,11 @@ function addMembershipCheckbox($user)
         <tr>
             <th><label for="gmss_membership">I would like to be a member</label></th>
             <td>
-                <input type="checkbox" name="gmss_membership" value="true" <?php
-                    if (get_the_author_meta('gmss_membership', $user->ID) == 'true')
-                        echo 'checked';
-                    ?> >
-                   By checking this box you will be counted as a member of the Greater Manchester Skeptics Society.
-                </input>
-            </td>
-        </tr>
-        <tr>
-            <th><label for="gmss_newsletter">I would like to receive the newsletter</label></th>
-            <td>
-                <input type="checkbox" name="gmss_newsletter" value="true" <?php
-                    if (get_the_author_meta('gmss_newsletter', $user->ID) == 'true')
-                        echo 'checked';
-                    ?> >
-                   By checking this box you will receive periodic email updates about the Society.
+            	<input type="checkbox" name="gmss_membership" value="true" <?php
+					if (get_the_author_meta('gmss_membership', $user->ID) == 'true')
+						echo 'checked';
+            		?> >
+            	   By checking this box you will be counted as a member of the Greater Manchester Skeptics Society, and receive periodic email updates about the Society.
                 </input>
             </td>
         </tr>
@@ -42,18 +31,11 @@ function addMembershipCheckbox($user)
 function saveMembershipPreference($user_id)
 {
     update_user_meta($user_id,
-        'gmss_membership',
-        sanitize_text_field($_POST['gmss_membership']));
-    update_user_meta($user_id,
-        'gmss_newsletter',
-        sanitize_text_field($_POST['gmss_newsletter']));
-    if (!get_the_author_meta('unsubscribe_key', $user->ID))
-        update_user_meta($user_id,
-            'unsubscribe_key',
-            strval(mt_rand(1000000, 9999999)));
+    	'gmss_membership',
+    	sanitize_text_field($_POST['gmss_membership']));
 }
 
-add_action('show_user_profile', 'addMembershipCheckbox');
-add_action('edit_user_profile', 'addMembershipCheckbox');
-add_action('personal_options_update', 'saveMembershipPreference');
-add_action('edit_user_profile_update', 'saveMembershipPreference');
+add_action( 'show_user_profile', 'addMembershipCheckbox' );
+add_action( 'edit_user_profile', 'addMembershipCheckbox' );
+add_action( 'personal_options_update', 'saveMembershipPreference' );
+add_action( 'edit_user_profile_update', 'saveMembershipPreference' );
