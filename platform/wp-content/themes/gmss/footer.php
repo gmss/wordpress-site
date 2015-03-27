508f2d0dd0acaed46de0c2762e84cb45201755b0
Andrew Taylor <mac@andrewt.net>
2015-03-27 22:09:24 +0000

    Added Humanists and hopefully fixed CSS

--- platform/wp-content/themes/gmss/footer.php
+++ platform/wp-content/themes/gmss/footer.php
@@ -15,6 +15,7 @@
 					<li><a href="http://www.qedcon.org/">QED: Question, Explore, Discover</a></li>
 					<li><a href="http://www.pubhdmanchester.org.uk/">PubhD Manchester</a></li>
 					<li><a href="https://sites.google.com/site/scibar/">Didsbury SciBar</a></li>
+					<li><a href="http://gmh.humanist.org.uk/">Greater Manchester Humanists</a></li>
 					<li><a href="http://www.mathsjam.com/index.php?city=manchester">Manchester MathsJam</a></li>
 					<li><a href="http://brightclubmcr.org.uk/">Bright Club Manchester</a></li>
 					<li><a href="http://pintofscience.co.uk/events/manchester/">Pint of Science Manchester</a></li>
